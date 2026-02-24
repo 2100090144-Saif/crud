9a0f3a96e0d19d11c4696878c93bb433c3acd209
Md Saif <114153525+2100090144-Saif@users.noreply.github.com>
2026-02-24 14:27:51 +0530

    Update MySQL password to a placeholder
    Replaced the MySQL password with a placeholder.

--- p3.py
+++ p3.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # MySQL Configuration
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Saif@9973'
+app.config['MYSQL_PASSWORD'] = 'xxxxxx'
 app.config['MYSQL_DB'] = 'crud_db'
 
 mysql = MySQL(app)
